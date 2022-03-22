fa16ec035ae426b5678b58e46f464642963604de
ThongSatoshi <hduthong25@gmail.com>
2022-03-22 12:49:49 +0700

    Temp 5
    Update Repo

--- Day 5 - HTML DOM and Event/Ex 2 - Calculator/Easy Calculator/doCal.js
+++ Day 5 - HTML DOM and Event/Ex 2 - Calculator/Easy Calculator/doCal.js
@@ -1,40 +1,59 @@
-let firstNum = document.getElementById("firstNum");
-let secondNum = document.getElementById("secondNum");
-let add = document.getElementById("addBtn");
-let sub = document.getElementById("subBtn");
-let mul = document.getElementById("mulBtn");
-let div = document.getElementById("divBtn");
-let doCal = document.getElementById("doCal");
-let clear = document.getElementById("resetBtn");
-let result = document.getElementById("result");
-var a, b, re;
-
-firstNum.addEventListener("change", function() {
-    a = parseFloat(firstNum.value);
-});
-secondNum.addEventListener("change", function() {
-    b = parseFloat(secondNum.value);
-});
-
-add.addEventListener("click", function() {
-    re = a + b;
-});
-sub.addEventListener("click", function() {
-    re = a - b;
-});
-mul.addEventListener("click", function() {
-    re = a * b;
-});
-div.addEventListener("click", function() {
-    re = a / b;
-});
-
-result.addEventListener("click", function() {
-    if (isNaN(re) || re == null) {
-        result.innerText = "[Error] không thể thực hiện phép tính giữa " + a + " và " + b;
-    } else {
-        result.innerText = "Kết quả phép tính giữa " + a + " và " + b + " là " + re;
-    };
-});
+windows.onload = function() {
+    let firstNum = document.getElementById("firstNum");
+    let secondNum = document.getElementById("secondNum");
+    let add = document.getElementById("addBtn");
+    let sub = document.getElementById("subBtn");
+    let mul = document.getElementById("mulBtn");
+    let div = document.getElementById("divBtn");
+    let clear = document.getElementById("resetBtn");
+    
+    add.addEventListener("click", function() {
+        let a = parseFloat(firstNum.value);
+        let b = parseFloat(secondNum.value);
+        let re;
+        re = a + b;
+        if (isNaN(re) || re == null) {
+            result.innerHTML = "[Error] Không thể thực hiện tính phép tính";
+        } else {
+            result.innerHTML = "Kết quả phép tính là: " + re;
+        };
+    });
+    
+    sub.addEventListener("click", function() {
+        let a = parseFloat(firstNum.value);
+        let b = parseFloat(secondNum.value);
+        let re;
+        re = a - b;
+        if (isNaN(re) || re == null) {
+            result.innerHTML = "[Error] Không thể thực hiện tính phép tính";
+        } else {
+            result.innerHTML = "Kết quả phép tính là: " + re;
+        };
+    });
+    
+    mul.addEventListener("click", function() {
+        let a = parseFloat(firstNum.value);
+        let b = parseFloat(secondNum.value);
+        let re;
+        re = a * b;
+        if (isNaN(re) || re == null) {
+            result.innerHTML = "[Error] Không thể thực hiện tính phép tính";
+        } else {
+            result.innerHTML = "Kết quả phép tính là: " + re;
+        };
+    });
+    
+    div.addEventListener("click", function() {
+        let a = parseFloat(firstNum.value);
+        let b = parseFloat(secondNum.value);
+        let re;
+        re = a / b;
+        if (isNaN(re) || re == null) {
+            result.innerHTML = "[Error] Không thể thực hiện tính phép tính";
+        } else {
+            result.innerHTML = "Kết quả phép tính là: " + re;
+        };
+    });
 
 clear.addEventListener("click", (firstNum && secondNum).reset());
+};
