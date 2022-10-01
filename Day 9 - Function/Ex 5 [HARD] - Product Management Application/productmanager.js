683438c1d681eb3c700150be2611fd942be5c073
ThongSatoshi <hduthong25@gmail.com>
2022-10-02 03:30:26 +0700

    Update Repo

--- Day 9 - Function/Ex 5 [HARD] - Product Management Application/productmanager.js
+++ Day 9 - Function/Ex 5 [HARD] - Product Management Application/productmanager.js
@@ -10,22 +10,21 @@ const TABLESIZE = 5;
 
 let cakeArr = [(new Item("Chocolate Cake", "25.75 USD", randomID())),
 (new Item("Strawberry Cake", "30.15 USD", randomID())),
-(new Item("Cheese Cake", "35.45 USD", randomID())),
-(new Item("Matcha Cake", "48.20 USD", randomID())),
-(new Item("Golden Cake", "45.00 USD", randomID())),
+(new Item("Blueberry Cheesecake", "35.45 USD", randomID())),
+(new Item("Matcha Layer Cake", "48.20 USD", randomID())),
+(new Item("The Silky Milky", "46.00 USD", randomID())),
 ];
 
-let breadArr = [(new Item("Banh Mi Sandwich", "2.55 USD", randomID())),
-(new Item("Hamburger and Fries", "3.25 USD", randomID())),
-(new Item("Katsu Sandwich", "5.00 USD", randomID())),
-(new Item("Fruit Sandwich", "3.50 USD", randomID())),
+let breadArr = [(new Item("Grilled Pork Sandwich", "2.55 USD", randomID())),
+(new Item("Shrimp Burger", "3.25 USD", randomID())),
+(new Item("Katsu Sando", "5.00 USD", randomID())),
 (new Item("The Big Boss", "6.00 USD", randomID())),
 ];
 
 let icecreamArr = [(new Item("Vanilla & Chocolate", "1.75 USD", randomID())),
 (new Item("Strawberry & Blueberry", "2.00 USD", randomID())),
-(new Item("Matcha Red Bean Paste", "5.25 USD", randomID())),
-(new Item("Tropical Trophy", "3.50 USD", randomID())),
+(new Item("Matcha Red Bean Paste", "4.25 USD", randomID())),
+(new Item("The Mango Trophy", "5.00 USD", randomID())),
 ];
 
 let drinkArr = [(new Item("Blue Marine", "2.25 USD", randomID())),
@@ -45,104 +44,123 @@ function randomID() {
 };
 
 function showCakeList() {
-    let board = document.getElementById("cakeList");
+    let itemList = document.getElementById("cakeList");
     let a = b = c = d = 0, colA = colB = colC = colD = 1;
     let grid = "<table class='productList'><tr>";
-    while (colA <= TABLESIZE || colA <= cakeArr.length) {
-        grid += "<td><img class='itemImage' src='\image\menu" + cakeArr[a].name + "'></td>"
+    while (colA <= TABLESIZE && a < cakeArr.length) {
+        grid += "<td><img class='itemImage' src='image\menu\ " + cakeArr[a].name + ".png' alt='Item Picture'></td>"
+        colA++, a++;
     };
     grid += "</tr>"
 
-    while (colB <= TABLESIZE || colB <= cakeArr.length) {
+    while (colB <= TABLESIZE && b < cakeArr.length) {
         grid += "<td>" + cakeArr[b].name + "</td>";
         colB++, b++;
     };
     grid += "</tr>"
 
-    while (colC <= TABLESIZE || colC <= cakeArr.length) {
+    while (colC <= TABLESIZE && c < cakeArr.length) {
         grid += "<td>" + cakeArr[c].price + "</td>";
         colC++, c++;
     };
     grid += "</tr>"
 
-    while (colD <= TABLESIZE || colD <= cakeArr.length) {
-        grid += "<td>" + cakeArr[d].id + "</td>";
+    while (colD <= TABLESIZE && d < cakeArr.length) {
+        grid += "<td>ID: " + cakeArr[d].id + "</td>";
         colD++, d++;
     };
     grid += "</tr></table>";
-    board.innerHTML = grid;
+    itemList.innerHTML = grid;
 };
 
 function showBreadList() {
-    let board = document.getElementById("breadList");
-    let i = j = k = 0, colA = colB = colC = 1;
+    let itemList = document.getElementById("breadList");
+    let a = b = c = d = 0, colA = colB = colC = colD = 1;
     let grid = "<table class='productList'><tr>";
-    while (colA <= TABLESIZE || colA <= breadArr.length) {
-        grid += "<td>" + breadArr[i].name + "</td>";
-        colA++, i++;
+    while (colA <= TABLESIZE && a < breadArr.length) {
+        grid += "<td><img class='itemImage' src='image\menu\ " + breadArr[a].name + ".png' alt='Item Picture'></td>"
+        colA++, a++;
     };
     grid += "</tr>"
 
-    while (colB <= TABLESIZE || colB <= breadArr.length) {
-        grid += "<td>" + breadArr[j].price + "</td>";
-        colB++, j++;
+    while (colB <= TABLESIZE && b < breadArr.length) {
+        grid += "<td>" + breadArr[b].name + "</td>";
+        colB++, b++;
     };
     grid += "</tr>"
 
-    while (colC <= TABLESIZE || colC <= breadArr.length) {
-        grid += "<td>" + breadArr[k].id + "</td>";
-        colC++, k++;
+    while (colC <= TABLESIZE && c < breadArr.length) {
+        grid += "<td>" + breadArr[c].price + "</td>";
+        colC++, c++;
+    };
+    grid += "</tr>"
+
+    while (colD <= TABLESIZE && d < breadArr.length) {
+        grid += "<td>ID: " + breadArr[d].id + "</td>";
+        colD++, d++;
     };
     grid += "</tr></table>";
-    board.innerHTML = grid;
+    itemList.innerHTML = grid;
 };
 
 function showIcecreamList() {
-    let board = document.getElementById("icecreamList");
-    let i = j = k = 0, colA = colB = colC = 1;
+    let itemList = document.getElementById("icecreamList");
+    let a = b = c = d = 0, colA = colB = colC = colD = 1;
     let grid = "<table class='productList'><tr>";
-    while (colA <= TABLESIZE || colA <= icecreamArr.length) {
-        grid += "<td>" + icecreamArr[i].name + "</td>";
-        colA++, i++;
+    while (colA <= TABLESIZE && a < icecreamArr.length) {
+        grid += "<td><img class='itemImage' src='image\menu\ " + icecreamArr[a].name + ".png' alt='Item Picture'></td>"
+        colA++, a++;
     };
     grid += "</tr>"
 
-    while (colB <= TABLESIZE || colB <= icecreamArr.length) {
-        grid += "<td>" + icecreamArr[j].price + "</td>";
-        colB++, j++;
+    while (colB <= TABLESIZE && b < icecreamArr.length) {
+        grid += "<td>" + icecreamArr[b].name + "</td>";
+        colB++, b++;
     };
     grid += "</tr>"
 
-    while (colC <= TABLESIZE || colC <= icecreamArr.length) {
-        grid += "<td>" + icecreamArr[k].id + "</td>";
-        colC++, k++;
+    while (colC <= TABLESIZE && c < icecreamArr.length) {
+        grid += "<td>" + icecreamArr[c].price + "</td>";
+        colC++, c++;
+    };
+    grid += "</tr>"
+
+    while (colD <= TABLESIZE && d < icecreamArr.length) {
+        grid += "<td>ID: " + icecreamArr[d].id + "</td>";
+        colD++, d++;
     };
     grid += "</tr></table>";
-    board.innerHTML = grid;
+    itemList.innerHTML = grid;
 };
 
 function showDrinkList() {
-    let board = document.getElementById("drinkList");
-    let i = j = k = 0, colA = colB = colC = 1;
+    let itemList = document.getElementById("drinkList");
+    let a = b = c = d = 0, colA = colB = colC = colD = 1;
     let grid = "<table class='productList'><tr>";
-    while (colA <= TABLESIZE || colA <= drinkArr.length) {
-        grid += "<td>" + drinkArr[i].name + "</td>";
-        colA++, i++;
+    while (colA <= TABLESIZE && a < drinkArr.length) {
+        grid += "<td><img class='itemImage' src='image\menu\ " + drinkArr[a].name + ".png' alt='Item Picture'></td>"
+        colA++, a++;
     };
     grid += "</tr>"
 
-    while (colB <= TABLESIZE || colB <= drinkArr.length) {
-        grid += "<td>" + drinkArr[j].price + "</td>";
-        colB++, j++;
+    while (colB <= TABLESIZE && b < drinkArr.length) {
+        grid += "<td>" + drinkArr[b].name + "</td>";
+        colB++, b++;
     };
     grid += "</tr>"
 
-    while (colC <= TABLESIZE || colC <= drinkArr.length) {
-        grid += "<td>" + drinkArr[k].id + "</td>";
-        colC++, k++;
+    while (colC <= TABLESIZE && c < drinkArr.length) {
+        grid += "<td>" + drinkArr[c].price + "</td>";
+        colC++, c++;
+    };
+    grid += "</tr>"
+
+    while (colD <= TABLESIZE && d < drinkArr.length) {
+        grid += "<td>ID: " + drinkArr[d].id + "</td>";
+        colD++, d++;
     };
     grid += "</tr></table>";
-    board.innerHTML = grid;
+    itemList.innerHTML = grid;
 };
 
 showCakeList();
