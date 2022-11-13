0054e69f816836724110b77c8352eff1d70e8dac
ThongSatoshi <hduthong25@gmail.com>
2022-11-13 23:58:50 +0700

    Update Repo

--- Day 10 - Canvas/Demo 2 - Animating a Rectangle/function.js
+++ Day 10 - Canvas/Demo 2 - Animating a Rectangle/function.js
@@ -1,8 +1,10 @@
-let smallBoatA = new Boat(smallBoatX, smallBoatY, smallBoatWidth, smallBoatHeight, 9, "rgb(190,190,190)"),
-    smallBoatB = new Boat(smallBoatX + 350, smallBoatY, smallBoatWidth, smallBoatHeight, 7, "rgb(190,190,190)");
-let bigBoatA = new Boat(bigBoatX, bigBoatY, bigBoatWidth, bigBoatHeight, 6, "rgb(192,192,192)"),
-    bigBoatB = new Boat(bigBoatX + 350, bigBoatY, bigBoatWidth, bigBoatHeight, 4);
-let player = new Player(50, 250, 30, 3, "rgb(255, 233, 220)");
+let smallBoatX = 200, smallBoatY = 0, bigBoatX = 380, bigBoatY = 300,
+    smallBoatWidth = smallBoatHeight = 100, bigBoatWidth = 100, bigBoatHeight = 200;
+let smallBoatA = new Boat(smallBoatX, smallBoatY, smallBoatWidth, smallBoatHeight, 5, "rgb(190,190,190)"),
+    smallBoatB = new Boat(smallBoatX + 350, smallBoatY, smallBoatWidth, smallBoatHeight, 5, "rgb(190,190,190)");
+let bigBoatA = new Boat(bigBoatX, bigBoatY, bigBoatWidth, bigBoatHeight, 3, "rgb(192,192,192)"),
+    bigBoatB = new Boat(bigBoatX + 350, bigBoatY, bigBoatWidth, bigBoatHeight, 3, "rgb(192,192,192)");
+let player = new Player(50, 250, 30, 10, "rgb(255, 233, 220)");
 drawBackground(), drawSmallBoat(), drawBigBoat(), drawPlayer();
 
 // Reset lại canvas
@@ -79,6 +81,10 @@ startButton.addEventListener("click", function () {
         return alert("Error: The game is running. No need to press the 'Start game now!' button again!");
     } else {
         runGame();
+        canvas.scrollIntoView(false);
+        window.addEventListener("scroll", function(){
+            canvas.scrollIntoView(false);
+        });
     };
 });
 
@@ -112,51 +118,49 @@ function runGame() {
         drawBigBoat();
     };
 
-    // Kiểm tra phím được ấn
-    var pressedKey;
-    window.addEventListener("keydown", function (event) {
-        return pressedKey = event.key;
-        playerMovement();
-    });
-
-    // Khởi tạo hoạt hình nhân vật
-    function playerMovement() {
-        switch (pressedKey) {
-            case "ArrowUp" || "W" || "w":
-                player.playerY -= player.playerSpeed;
-                break;
-            case "ArrowDown" || "S" || "s":
-                player.playerY += player.playerSpeed;
-                break;
-            case "ArrowLeft" || "A" || "a":
-                player.playerX -= player.playerSpeed;
-                break;
-            case "ArrowRight" || "D" || "d":
-                player.playerX += player.playerSpeed;
-                break;
-        };
-
-        if (player.playerY <= player.playerSize) {
-            player.playerY += player.playerSize;
-        } else if ((player.playerY + player.playerSize) >= canvas.height) {
-            player.playerY -= player.playerSize;
-        };
-        if (player.playerX <= player.playerSize) {
-            player.playerX += player.playerSize;
-        } else if ((player.playerX + player.playerSize) >= canvas.width) {
-            player.playerX -= player.playerSize;
-        };
-
-        drawPlayer();
-    };
-
     // Lặp các hàm cần thiết
     setInterval(drawBackground, frame);
     setInterval(smallBoatAnimation, frame);
     setInterval(bigBoatAnimation, frame);
-    setInterval(playerMovement, frame);
-    // setInterval(isWinning, 1000);
-    // setInterval(isDead, 1000);
+    setInterval(drawPlayer, frame);
+    setInterval(checkWinner, frame);
+    setInterval(checkCollision, frame);
+};
+
+// Kiểm tra phím được ấn
+var pressedKey;
+window.addEventListener("keydown", function (event) {
+    pressedKey = event.key;
+    return playerMovement();
+});
+
+// Khởi tạo hoạt hình nhân vật
+function playerMovement() {
+    switch (pressedKey) {
+        case "ArrowUp" || "W" || "w":
+            player.playerY -= player.playerSpeed;
+            break;
+        case "ArrowDown" || "S" || "s":
+            player.playerY += player.playerSpeed;
+            break;
+        case "ArrowLeft" || "A" || "a":
+            player.playerX -= player.playerSpeed;
+            break;
+        case "ArrowRight" || "D" || "d":
+            player.playerX += player.playerSpeed;
+            break;
+    };
+
+    if (player.playerY <= player.playerSize) {
+        player.playerY += player.playerSize;
+    } else if ((player.playerY + player.playerSize) >= canvas.height) {
+        player.playerY -= player.playerSize;
+    };
+    if (player.playerX <= player.playerSize) {
+        player.playerX += player.playerSize;
+    } else if ((player.playerX + player.playerSize) >= canvas.width) {
+        player.playerX -= player.playerSize;
+    };
 };
 
 // Reset tất cả về trạng thái ban đầu
@@ -168,12 +172,24 @@ resetButton.addEventListener("click", function () {
     };
 });
 
-// // Kiểm tra xem nhân vật đã về đích?
-// function isWinning() {
+// Kiểm tra xem nhân vật đã về đích?
+function checkWinner() {
+    if (isWinning == false) {
+        if ((player.playerX + player.playerSize) > (940 + player.playerSize * 2)) {
+            isWinning = true;
+            alert("Congratulation. The soldier has crossed the river safely!");
+            let isReset = confirm("Do you want to restart this game again?");
+            if (isReset == true) {
+                isRunning = isWinning = false;
+                return location.reload();
+            } else {
+                return;
+            };
+        };
+    };
+};
 
-// };
-
-// // Kiểm tra xem nhân vật va chạm các thuyền?
-// function isDead() {
-
-// };
+// Kiểm tra xem nhân vật va chạm các thuyền?
+function checkCollision() {
+    
+};
