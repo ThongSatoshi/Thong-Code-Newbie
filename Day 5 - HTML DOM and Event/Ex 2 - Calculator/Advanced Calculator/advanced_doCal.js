fbeac68bdb3a5174c57712c9e74304095592f68a
ThongSatoshi <hduthong25@gmail.com>
2023-07-19 01:48:40 +0700

    Update Repo

--- Day 5 - HTML DOM and Event/Ex 2 - Calculator/Advanced Calculator/advanced_doCal.js
+++ Day 5 - HTML DOM and Event/Ex 2 - Calculator/Advanced Calculator/advanced_doCal.js
@@ -1,10 +1,38 @@
-let operator = document.querySelectorAll(".operatorBtn");
-let number = nodeList = document.querySelectorAll(".numPad");
-let firstNum = [], secondNum = [];
-let result;
+let screen = document.getElementById("screen");
+let operator = document.getElementsByClassName("operatorBtn").value;
+let result, firstNumArr = [], secondNumArr = [], firstNum = 0, secondNum = 0, isNextNum = false;
 
-number.addEventListener ("click", function() {
-    for(let i = 0; i < number.length; i++) {
-        console.log(number[i]); 
-    };   
-});
+function inputNum(value) {
+    if (firstNumArr.length == 0 || isNextNum == false) {
+        firstNumArr.push(value);
+        for (let i = 0; i < firstNumArr.length; i++) {
+            screen.innerHTML = firstNumArr.join("");
+        };
+    } else {
+        secondNumArr.push(value);
+        for (let i = 0; i < secondNumArr.length; i++) {
+            screen.innerHTML += secondNumArr.join("");
+        };
+    };
+    // console.log(firstNumArr, secondNumArr);
+};
+
+function inputOperator(operatorType) {
+    console.log(operatorType);
+    switch (operatorType) {
+        case 1:
+            if (firstNumArr.length != 0) {
+                isNextNum = true;
+                screen.innerHTML += "+";
+            } else {
+                break;
+            };
+            break;
+
+    }
+};
+
+function outputResult() {
+
+    console.log("a result!");
+};
