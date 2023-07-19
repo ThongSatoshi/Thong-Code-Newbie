65e0c478088b9f4b8dc20e5b9097271ff63a00f5
ThongSatoshi <hduthong25@gmail.com>
2023-07-19 13:46:09 +0700

    Update Repo

--- Day 5 - HTML DOM and Event/Ex 2 - Calculator/Advanced Calculator/advanced_doCal.js
+++ Day 5 - HTML DOM and Event/Ex 2 - Calculator/Advanced Calculator/advanced_doCal.js
@@ -1,35 +1,62 @@
 let screen = document.getElementById("screen");
-let operator = document.getElementsByClassName("operatorBtn").value;
-let result, firstNumArr = [], secondNumArr = [], firstNum = 0, secondNum = 0, isNextNum = false;
+let result, operator, inputValue;
+let inputValueArr = [], savedValue = [], savedOperator = [];
+let numCount = 0, negativeSign = firstOperation = "";
+
+function getElementbyId(id) {
+    return document.getElementById(id).innerHTML;
+}
 
 function inputNum(value) {
-    if (firstNumArr.length == 0 || isNextNum == false) {
-        firstNumArr.push(value);
-        for (let i = 0; i < firstNumArr.length; i++) {
-            screen.innerHTML = firstNumArr.join("");
-        };
+    let str = "num" + value + "Btn";
+    let input = getElementbyId(str);
+    inputValueArr.push(input);
+    inputValue = negativeSign + inputValueArr.join("");
+    savedValue.splice(numCount, 1, inputValue);
+    if (numCount < 1) {
+        screen.innerHTML = savedValue[numCount];
     } else {
-        secondNumArr.push(value);
-        for (let i = 0; i < secondNumArr.length; i++) {
-            screen.innerHTML += secondNumArr.join("");
-        };
+        screen.innerHTML = firstOperation + savedValue[numCount];
     };
-    // console.log(firstNumArr, secondNumArr);
 };
 
 function inputOperator(operatorType) {
-    console.log(operatorType);
     switch (operatorType) {
         case 1:
-            if (firstNumArr.length != 0) {
-                isNextNum = true;
-                screen.innerHTML += "+";
+            operator = " + ";
+            savedOperator.splice(numCount, 1, operator);
+            firstOperation = screen.innerHTML += savedOperator[numCount];
+            numCount++, negativeSign = "", inputValueArr = [];
+            break;
+        case 2:
+            if (savedValue[0] === undefined) {
+                negativeSign = " -";
+                screen.innerHTML = negativeSign;
+                break;
+            } else if (savedValue[numCount] === undefined && savedOperator[numCount - 1] !== undefined) {
+                negativeSign = " -";
+                screen.innerHTML = firstOperation + negativeSign;
+                break;
             } else {
+                operator = " - ";
+                savedOperator.splice(numCount, 1, operator);
+                firstOperation = screen.innerHTML += savedOperator[numCount];
+                numCount++, negativeSign = "", inputValueArr = [];
                 break;
             };
+        case 3:
+            operator = " x ";
+            savedOperator.splice(numCount, 1, operator);
+            firstOperation = screen.innerHTML += savedOperator[numCount];
+            numCount++, negativeSign = "", inputValueArr = [];
             break;
-
-    }
+        case 4:
+            operator = " : ";
+            savedOperator.splice(numCount, 1, operator);
+            firstOperation = screen.innerHTML += savedOperator[numCount];
+            numCount++, negativeSign = "", inputValueArr = [];
+            break;
+    };
 };
 
 function outputResult() {
